a123280923aa96974a28e044ade4207f8a5f25c7
Estebanmg58 <56441614+Estebanmg58@users.noreply.github.com>
2020-04-14 10:50:32 -0500

    Aqui para ver unos avances breves

---
+++ PracticaJS/JsAdvance.js
@@ -0,0 +1,10 @@
+const cat = {
+    age: 2,
+    name: 'Bailey',
+    meow: function () {
+      console.log('Meow!');
+    },
+    greet: function (name) {
+      console.log(`Hello ${name}`);
+    }
+  };
